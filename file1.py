2755aa6c8606be540eda4f9fb8397ca4918cd5c8
Thasminanissa <42801326+Thasminanissa@users.noreply.github.com>
2018-09-26 09:34:42 +0530

    Update and rename file1.py to file2.py

--- file1.py
+++
@@ -1,8 +0,0 @@
-
-man=int(input())
-temp=abs(man)
-sum=0
-while(temp!=0):
-    temp=int(temp/10)
-    sum=sum+1
-print (sum)
---
+++ file2.py
@@ -0,0 +1,5 @@
+cat=int(input())
+sum=0
+for i in range(1,cat+1):
+    sum=sum+i
+print(sum)
