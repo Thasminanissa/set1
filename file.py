58e0d9f64e435514f8803748e9497f8ede1b7f39
Thasminanissa <42801326+Thasminanissa@users.noreply.github.com>
2018-09-26 09:29:34 +0530

    Update and rename file.py to file1.py

--- file.py
+++
@@ -1,4 +0,0 @@
-s=0
-t=int(input())
-for s in range(s,t):
-    print("Hello")
---
+++ file1.py
@@ -0,0 +1,8 @@
+
+man=int(input())
+temp=abs(man)
+sum=0
+while(temp!=0):
+    temp=int(temp/10)
+    sum=sum+1
+print (sum)
